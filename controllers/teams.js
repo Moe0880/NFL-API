b7dd40377fa1d96e8cffe425eca18e1a1ece77e8
Moe <moesoliman80@gmail.com>
2020-08-31 21:03:59 -0400

    I am Batman

--- controllers/teams.js
+++ controllers/teams.js
@@ -3,7 +3,7 @@ const getTeams = (req, res) => {
   return res.send(teams);
 };
 const getTeamById = (req, res) => {
-  const { id } = request.params;
+  const { id } = req.params;
   const foundTeam = teams.filter((team) => team.id === id);
   return res.send(foundTeam);
 };
